58ba3d4348a65c139282a52f0267443d98b501e5
rrjoson <rr_joson@yahoo.com>
2017-05-02 01:48:05 +0800

    Add RepositoryListItem

---
+++ src/components/RepositoryListItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RepositoryListItem extends Component {
+  render() {
+    return (
+      <div>RepositoryListItem</div>
+    );
+  }
+}
+
+export default RepositoryListItem;
