421dc3a7941ed6a0c456fdbdae3d7aa9706c3f6f
rrjoson <rr_joson@yahoo.com>
2017-05-02 01:55:10 +0800

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>React JS Tutorial</h1>
-        <h2>Playing with React</h2>
+      <div className="section">
+        <h1><a className="title is-1" href="#">Gucode - ReactJS Tutorial</a></h1>
+        <h2 className="subtitle is-3">Playing with React</h2>
       </div>
     );
   }
