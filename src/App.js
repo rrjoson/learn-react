24cdec51c05c8c605f1f138dbcabf36ea0179b26
rrjoson <rr_joson@yahoo.com>
2017-05-02 01:30:39 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Dashboard />
+      </div>
+    );
+  }
+}
+
+export default App;
